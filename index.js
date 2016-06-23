a3b04e091aac9f611cb61f83a00ebcd821915d30
Jake Boyles <jake@jibdesigns.com>
2016-06-22 22:12:09 -0400

    fixing spacing

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ if (process.env.NODE_ENV !== 'production') {
 
 const server = require('./server');
 const PORT = process.env.PORT || 3000;
-
 server.listen(PORT, () => {
   console.log('The CincyBrewScene server is running.');
 });
